e426595ccdeabd7501c3250c5aec25470123044f
kavishka-sulakshana <kss20001218@gmail.com>
2023-07-01 02:52:39 +0530

    add redux to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Header from './components/Header/Header'
+import Home from './pages/Home/Home'
 
 
-function App () {
-
+function App() {
 
   return (
     <Router>
       <div>
-        <Header />
         <Routes>
-          <Route path='#' element={<Header />} />
+          <Route path='/' element={<Home />} />
         </Routes>
       </div>
     </Router>
 
-    
+
 
   )
 }
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../components/Header/Header'
+import { Counter } from '../../components/counter/Counter.jsx'
+
+const Home = () => {
+    return (
+        <>
+            <Header />
+            <Counter />
+        </>
+    )
+}
+
+export default Home
