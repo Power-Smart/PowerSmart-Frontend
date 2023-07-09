259f8752b93f7d74ed13b74029577654b76b1f67
virajSandakelum <2020cs011@stu.ucsc.cmb.ac.lk>
2023-07-09 09:44:26 +0530

    delete table component from main

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
 import Register from './pages/Register/Register'
-import Table from './components/Table/Table'
 
 
 
