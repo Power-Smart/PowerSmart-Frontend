8fc20250b769f62f979854fc859f916407ae1ff6
kavishka-sulakshana <kss20001218@gmail.com>
2023-07-15 22:25:59 +0530

    fix sidebar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import Header from '../../components/Header/Header'
 import Sidebar from '../../components/Sidebar/Sidebar'
 
 const Home = () => {
     return (
         <>
-            <Sidebar />
-            {/* <Header /> */}
         </>
     )
 }
