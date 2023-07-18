d6246a917254c0e60b9a79032c81c5602550aed6
virajSandakelum <2020cs011@stu.ucsc.cmb.ac.lk>
2023-07-18 15:58:04 +0530

    set the table

--- src/pages/TechSupport/CustomerRequestTable/Table.jsx
+++ src/pages/TechSupport/CustomerRequestTable/Table.jsx
@@ -6,14 +6,14 @@ const dataset = [
         complaint: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.',
         userID: 'John Doe',
         assigned: '10',
-        status: 'Pending'
+        status: '05'
     },
     {
         id: '#413656',
         complaint: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.',
         userID: 'Alice Smith',
         assigned: 'Bob Johnson',
-        status: 'Rejected'
+        status: 'Resolved'
     },
     {
         id: '#413656',
@@ -46,12 +46,6 @@ const Table2 = () => {
             <table className="w-full text-sm text-left text-gray-500">
                 <thead className="text-xs text-gray-700 uppercase ">
                     <tr>
-                        {/* <th scope="col" className="p-4">
-                            <div className="flex items-center">
-                                <input id="checkbox-all-search" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                                <label htmlFor="checkbox-all-search" className="sr-only">checkbox</label>
-                            </div>
-                        </th> */}
                         <th scope="col" className="px-6 py-3">
                             Customer ID
                         </th>
--- src/pages/TechSupport/CustomerRequestTable/TableRow.jsx
+++ src/pages/TechSupport/CustomerRequestTable/TableRow.jsx
@@ -4,17 +4,11 @@ import profilePic from '../../../assets/images/profile.jpeg'
 const Table2Row = ({ id, complaint, userID, assigned, status }) => {
     return (
         <tr className="border-b dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-900">
-            {/* <td className="w-4 p-4">
-                <div className="flex items-center">
-                    <input id="checkbox-table-search-1" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                    <label htmlFor="checkbox-table-search-1" className="sr-only">checkbox</label>
-                </div>
-            </td> */}
             <th scope="row" className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
                 {/* <img className="w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-1.jpg" alt="Jese image" /> */}
                 <div className="pl-3">
                     <div className="text-base font-semibold">{id}</div>
-                    {/* <div className="font-normal text-gray-500">neil.sims@flowbite.com</div> */}
+                    <div className="font-normal text-gray-500">neil.sims@flowbite.com</div>
                 </div>
             </th>
             <td className="px-6 py-4">
@@ -27,21 +21,11 @@ const Table2Row = ({ id, complaint, userID, assigned, status }) => {
                 {assigned}
             </td>
             <td className="px-6 py-4">
-                <div className="flex items-center justify-center">
-                    {/* <div className="h-2.5 w-2.5 rounded-full bg-green-500 mr-2"></div> {status} */}
-                    {status === 'Pending' && <span className="px-2 py-1 inline-flex text-xs leading-5 rounded-full bg-yellow-950 text-yellow-500">
-                        {status}
-                    </span>}
-                    {status === 'Resolved' && <span className="px-2 py-1 inline-flex text-xs leading-5 rounded-full bg-green-950 text-green-500">
-                        {status}
-                    </span>}
-                    {status === 'Rejected' && <span className="px-2 py-1 inline-flex text-xs leading-5 rounded-full bg-red-950 text-red-500">
-                        {status}
-                    </span>}
-                </div>
+                {status}
             </td>
             <td className="px-6 py-4">
-                <a href="#" type="button" data-modal-target="editUserModal" data-modal-show="editUserModal" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit user</a>
+                {status === 'Pending' && <button className='cancel__btn'>Cancel</button>}
+                {status === 'Resolved' && <button className='request__btn'>Request</button>}
             </td>
         </tr>
     )
