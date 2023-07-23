4595cc33dc09473541c73ed3e17cd520dae5b0db
virajSandakelum <2020cs011@stu.ucsc.cmb.ac.lk>
2023-07-23 18:35:07 +0530

    design the login register

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import React, { useState } from 'react'
-import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
 import Register from './pages/Register/Register'
-import Table from './components/ExampleTable/Table2'
 
 
 
@@ -14,7 +11,7 @@ function App() {
     <Router>
       <div className='flex h-screen items-center justify-center w-full'>
         <Routes>
-          <Route path='/' element={<Table />} />
+          <Route path='/' element={<Home />} />
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
         </Routes>
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -20,94 +20,94 @@ const Register = () => {
     };
 
     return (
-        <div className="login__page" >
-
-            <div className="login__form">
-                <h1>REGISTER</h1>
-                <Form
-                    name="normal_login"
-                    className="login-form"
-                    initialValues={{
-                        remember: true,
-                    }}
-                    onFinish={onFinish}
-                >
-                    <label htmlFor="">Name</label>
-                    <Form.Item
-                        name="name"
-                        rules={[
-                            {
-                                required: true,
-                                message: 'Please input your name',
-                            },
-                        ]}
+        <div className="register__page">
+            <div className="login__container" >
+                <div className="login__form">
+                    <h1>REGISTER</h1>
+                    <Form
+                        name="normal_login"
+                        className="login-form"
+                        initialValues={{
+                            remember: true,
+                        }}
+                        onFinish={onFinish}
                     >
-                        <Input prefix={<UserOutlined className="site-form-item-icon" />} />
-                    </Form.Item>
+                        <label htmlFor="">Name</label>
+                        <Form.Item
+                            name="name"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your name',
+                                },
+                            ]}
+                        >
+                            <Input prefix={<UserOutlined className="site-form-item-icon" />} />
+                        </Form.Item>
 
-                    <label htmlFor="">Email</label>
-                    <Form.Item
-                        name="email"
-                        rules={[
-                            {
-                                required: true,
-                                message: 'Please input your Username!',
-                            },
-                        ]}
-                    >
-                        <Input prefix={<UserOutlined className="site-form-item-icon" />} />
-                    </Form.Item>
+                        <label htmlFor="">Email</label>
+                        <Form.Item
+                            name="email"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your Username!',
+                                },
+                            ]}
+                        >
+                            <Input prefix={<UserOutlined className="site-form-item-icon" />} />
+                        </Form.Item>
 
-                    <label htmlFor="">Password</label>
-                    <Form.Item
-                        name="password"
-                        rules={[
-                            {
-                                required: true,
-                                message: 'Please input your Password!',
-                            },
-                        ]}
-                    >
-                        <Input
-                            prefix={<LockOutlined className="site-form-item-icon" />}
-                            type="password"
-                        />
-                    </Form.Item>
+                        <label htmlFor="">Password</label>
+                        <Form.Item
+                            name="password"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your Password!',
+                                },
+                            ]}
+                        >
+                            <Input
+                                prefix={<LockOutlined className="site-form-item-icon" />}
+                                type="password"
+                            />
+                        </Form.Item>
 
-                    <label htmlFor="">Confirm Password</label>
-                    <Form.Item
-                        name="confirmPassword"
-                        rules={[
-                            {
-                                required: true,
-                                message: 'Please input your Password!',
-                            },
-                        ]}
-                    >
-                        <Input
-                            prefix={<LockOutlined className="site-form-item-icon" />}
-                            type="password"
-                        />
-                    </Form.Item>
+                        <label htmlFor="">Confirm Password</label>
+                        <Form.Item
+                            name="confirmPassword"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your Password!',
+                                },
+                            ]}
+                        >
+                            <Input
+                                prefix={<LockOutlined className="site-form-item-icon" />}
+                                type="password"
+                            />
+                        </Form.Item>
 
 
-                    <Form.Item className='form__submission'>
-                        <div className="login__register">
-                            <Button type="primary" htmlType="submit" className="login-form-button">
-                                Register
-                            </Button><br></br>
-                        </div>
-                        <div className="google__login">
-                            Already Logged In?<a href=""> Login</a>
-                        </div>
-                    </Form.Item>
-                </Form>
+                        <Form.Item className='form__submission'>
+                            <div className="login__register">
+                                <Button type="primary" htmlType="submit" className="login-form-button">
+                                    Register
+                                </Button><br></br>
+                            </div>
+                            <div className="google__login">
+                                Already Logged In?<a href=""> Login</a>
+                            </div>
+                        </Form.Item>
+                    </Form>
+                </div>
+
+                <div className="image">
+                    <img src={registerPageImage} alt="" />
+                </div>
             </div>
-
-            <div className="image">
-                <img src={registerPageImage} alt="" />
-            </div>
-
         </div>
     )
 }
