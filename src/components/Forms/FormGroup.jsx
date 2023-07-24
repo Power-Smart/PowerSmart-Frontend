9e9d8c153b18ffbda36c75313dbc15fe552941c4
kavishka-sulakshana <kss20001218@gmail.com>
2023-07-24 19:59:13 +0530

    Create Form Components

---
+++ src/components/Forms/FormGroup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FormGroup = ({ children }) => {
+    return (
+        <div className="relative z-0 w-full mb-6 group">
+            {children}
+        </div>
+    )
+}
+
+export default FormGroup
