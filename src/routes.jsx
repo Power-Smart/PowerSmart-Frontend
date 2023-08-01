20428732b25581efd448e824da44ac7ba71ba626
kavishka-sulakshana <kss20001218@gmail.com>
2023-08-01 17:33:26 +0530

    modify routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -10,14 +10,54 @@ import CheckStatus from './pages/Customer/Rooms/CheckStatus'
 import BuyingCart from './pages/Customer/Billing/BuyingCart'
 import Profile from './pages/Customer/Profile/Profile'
 import CustomerDashboard from './pages/Customer/Dashboard/Dashboard'
+import Protected from "./pages/Customer/Protected/Protected";
 
 export const routesArray = [
     {
         path: "/",
+        element: <Protected />,
+        children: [
+            {
+                path: "/",
+                element: <CustomerDashboard />,
+            },
+            {
+                path: "places",
+                element: <Places />,
+            },
+            {
+                path: "rooms",
+                element: <Rooms />,
+            },
+            {
+                path: "rooms/:id",
+                element: <InsideRoom />,
+            },
+            {
+                path: "check-status",
+                element: <CheckStatus />,
+            },
+            {
+                path: "buying-cart",
+                element: <BuyingCart />,
+            },
+            {
+                path: "profile",
+                element: <Profile />,
+            },
+        ]
+    },
+    {
+        path: "register",
         element: <Register />,
     },
     {
         path: "login",
         element: <Login />,
     },
+    {
+        path: "home",
+        element: <Home />,
+    }
+
 ];
