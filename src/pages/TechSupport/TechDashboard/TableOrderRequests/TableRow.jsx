62b95ff3a3e1e739c9588a42a7fa83f178793573
kavishka-sulakshana <kss20001218@gmail.com>
2023-08-11 14:39:25 +0530

    complete customer dashboard

---
+++ src/pages/TechSupport/TechDashboard/TableOrderRequests/TableRow.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const TableRow = ({ id, name, places, rooms }) => {
+    return (
+        <tr className="border-b dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-900">
+            {/* <td className="w-4 p-4">
+                <div className="flex items-center">
+                    <input id="checkbox-table-search-1" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                    <label htmlFor="checkbox-table-search-1" className="sr-only">checkbox</label>
+                </div>
+            </td> */}
+            <th scope="row" className="flex items-center px-2 py-4 text-gray-900 whitespace-nowrap dark:text-white">
+                {/* <img className="w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-1.jpg" alt="Jese image" /> */}
+                <div className="pl-3">
+                    <div className="text-base font-semibold">{id}</div>
+                    {/* <div className="font-normal text-gray-500">neil.sims@flowbite.com</div> */}
+                </div>
+            </th>
+            <td className="px-6 py-4">
+                {name}
+            </td>
+            <td className="px-6 py-4">
+                {places}
+            </td>
+            <td className="px-6 py-4">
+                {rooms}
+            </td>
+            <td className="px-6 py-4">
+                <a href="#" type="button" data-modal-target="editUserModal" data-modal-show="editUserModal" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">View</a>
+            </td>
+        </tr>
+    )
+}
+
+export default TableRow
--- src/pages/TechSupport/TechDashboard/TechDashboard.jsx
+++ src/pages/TechSupport/TechDashboard/TechDashboard.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import { TbDeviceSdCard } from 'react-icons/tb'
+import PageWrapper from '../../../components/Wrappers/PageWrapper'
+import MainSidebar from '../../../components/Sidebar/Customer/MainSidebar'
+import ContentWrapper from '../../../components/Wrappers/ContentWrapper'
+import PageContent from '../../../components/Wrappers/PageContent'
+import TopBar from '../../../components/smallComps/TopBar'
+import DashboardCard from './DashboardCard'
+import TableOrderRequests from './TableOrderRequests/TableOrderRequests'
+import TableOrderStatus from './TableOrderStatus/TableOrderStatus'
 
 const TechDashboard = () => {
     return (
-        <div>TechDashboard</div>
+        <PageWrapper>
+            <MainSidebar />
+            <PageContent>
+                <TopBar />
+                <ContentWrapper>
+                    <div className='flex justify-around m-1'>
+                        <div className='flex flex-col grow w-4/5 mx-auto'>
+                            <div className='flex justify-around items-center'>
+                                <DashboardCard title='Order Request' number={12} icon={<TbDeviceSdCard className='text-2xl' />} />
+                                <DashboardCard title='Product Request' number={12} icon={<TbDeviceSdCard className='text-2xl' />} />
+                                <DashboardCard title='Service Request' number={12} icon={<TbDeviceSdCard className='text-2xl' />} />
+                            </div>
+                            <div className='flex flex-col my-4'>
+                                <h1 className='text-lg font-semibold my-3'>Request Order</h1>
+                                <TableOrderRequests />
+                            </div>
+                            <div className='flex flex-col my-4'>
+                                <h1 className='text-lg font-semibold my-3'>Order Status</h1>
+                                <TableOrderStatus />
+                            </div>
+                        </div>
+                        <div className='flex flex-col w-32'>
+                            <h1 className='text-base text-center font-semibold'>Customer Feedback</h1>
+                        </div>
+                    </div>
+                </ContentWrapper>
+            </PageContent>
+        </PageWrapper>
     )
 }
 
