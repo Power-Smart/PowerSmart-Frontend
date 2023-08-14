4386485253196f57e996346595103724248c3295
virajSandakelum <2020cs011@stu.ucsc.cmb.ac.lk>
2023-08-14 23:29:43 +0530

    button custermize of the payment summary page

---
+++ src/components/Sidebar/TechSupport/MainSidebar.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import SidebarButton from '../SidebarButton'
+import { AiOutlineHome } from 'react-icons/ai'
+import { MdOutlinePlace } from 'react-icons/md'
+import { TbReport } from 'react-icons/tb'
+import { useState } from 'react'
+import { BsChatDots } from 'react-icons/bs'
+import { BiSupport } from 'react-icons/bi'
+import { RiMoneyDollarCircleLine } from 'react-icons/ri'
+import SidebarWrapper from '../SidebarWrapper'
+
+const MainSidebar = () => {
+
+    const [toggle, setToggle] = useState(true);
+
+    return (
+        <SidebarWrapper toggle={toggle} setToggle={setToggle}>
+            <SidebarButton text="Dashboard" toggle={toggle} link='/'>
+                <AiOutlineHome />
+            </SidebarButton>
+            <SidebarButton text="Places" toggle={toggle} link='/places'>
+                <MdOutlinePlace />
+            </SidebarButton>
+            {/* <SidebarButton text="Complaints" toggle={toggle} link='/Complaints'>
+                <TbReport />
+            </SidebarButton> */}
+            <SidebarButton text="Chat" toggle={toggle} link='/chat'>
+                <BsChatDots />
+            </SidebarButton>
+            <SidebarButton text="Payment" toggle={toggle} link='/payments'>
+                <RiMoneyDollarCircleLine />
+            </SidebarButton>
+            <SidebarButton text="Support" toggle={toggle} link='/support'>
+                <BiSupport />
+            </SidebarButton>
+        </SidebarWrapper>
+
+    )
+}
+
+export default MainSidebar
--- src/pages/TechSupport/PaymentSummary/PaymentSummary.jsx
+++ src/pages/TechSupport/PaymentSummary/PaymentSummary.jsx
@@ -5,7 +5,7 @@ import Sidebar from '../../../components/Sidebar/Sidebar'
 import TopBar from '../../../components/smallComps/TopBar'
 import PageContent from '../../../components/Wrappers/PageContent'
 import SelectedItem from './SelectedItem'
-
+import { Link } from 'react-router-dom'
 
 const PaymentSummary = () => {
     return (
@@ -22,14 +22,16 @@ const PaymentSummary = () => {
                     <SelectedItem itemName={'Relay 1'} itemPrice={'Rs. 1000.00'} place={'Bedroom'} room={'Room 1'} count={1} />
 
 
-                    <div className="do-payment rounded-md bg-[#3F3F46] p-2">
+                    <div className="do-payment rounded-md bg-[#3F3F46] p-2 w-full text-center">
                         <div className="flex justify-end items-center">
                             <p className="text-xl font-bold mr-4">Total</p>
-                            <p className="text-xl font-bold">Rs. 1000.00</p>
+                            <p className="text-xl font-bold pr-3">Rs. 1000.00</p>
                         </div>
-                        <div className="button-section w-full text-center m-auto flex justify-end align-middle mt-4">
-                            <button className='mx-2 px-4 py-2 bg-[#CE4444] rounded-md text-black w-20 font-bold'>Cancel</button>
-                            <button className='mx-2 px-4 py-2 bg-[#83BCFF] rounded-md text-black w-20 font-bold'>Pay</button>
+                        <div className="button-section flex justify-end align-middle mt-4">
+                            <button className='mx-2 px-4 py-2 bg-[#CE4444] rounded-md text-black w-fit font-bold'>Cancel</button>
+                            <Link to="/tech/accessCusAccount">
+                                <button className='mx-2 px-4 py-2 bg-[#83BCFF] rounded-md text-black w-fit font-bold'>Sent to customer</button>
+                            </Link>
                         </div>
                     </div>
 
