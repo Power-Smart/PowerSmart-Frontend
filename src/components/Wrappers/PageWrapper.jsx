d2b093adc082a0eca72769090d4962e897ce81b7
lahiru1115 <lahirudissanayake15@gmail.com>
2023-11-01 06:32:39 +0530

    change components

--- src/components/Wrappers/PageWrapper.jsx
+++ src/components/Wrappers/PageWrapper.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const PageWrapper = ({ children }) => {
-    return (
-        <section className='h-screen flex w-full overflow-hidden'>
-            {children}
-        </section>
+  return (
+    <section className="h-screen flex w-full overflow-hidden">
+      {children}
+    </section>
+  );
+};
 
-    )
-}
-
-export default PageWrapper
+export default PageWrapper;
--- src/components/smallComps/TopBar.jsx
+++ src/components/smallComps/TopBar.jsx
@@ -1,26 +1,32 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { IoChevronBack } from "react-icons/io5";
 
-import { Link } from 'react-router-dom'
-import { IoChevronBack } from 'react-icons/io5'
+const TopBar = ({ title, image, backLink = "" }) => {
+  return (
+    <div className="px-16 py-5 flex justify-between items-center">
+      <div className="flex items-center justify-center">
+        {backLink !== "" && (
+          <Link to={backLink}>
+            <div className="mr-4 font-bold"> {"<"} </div>
+            <IoChevronBack className="mr-4 h-7 w-7 text-xl font-bold bg-white/10 rounded-md" />
+          </Link>
+        )}
+        <h2 className="text-3xl font-bold">{title}</h2>
+      </div>
+      {/* <div className="flex items-center justify-center">
+        Profile Pic
+        <div>
+          <img
+            src={image}
+            alt="profile"
+            className="w-10 h-10 rounded-full border-[#83BCFF] border-2"
+          />
+        </div>
+      </div> */}
+    </div>
+  );
+};
 
-const TopBar = ({ title, image, baclLink = "" }) => {
-    return (
-        <div className='px-16 py-5 flex justify-between items-center' >
-            <div className='flex items-center justify-center'>
-                {baclLink != "" && <Link to={baclLink} >
-                    {/* <div className='mr-4 font-bold'> {"<"} </div> */}
-                    < IoChevronBack className='mr-4 h-7 w-7 text-xl font-bold bg-white/10 rounded-md' />
-                </Link>}
-                <h2 className='text-3xl font-bold'>{title}</h2>
-            </div>
-            <div className='flex items-center justify-center'>
-                {/* Profile Pic */}
-                {/* <div>
-                    <img src={image} alt="profile" className='w-10 h-10 rounded-full border-[#83BCFF] border-2' />
-                </div> */}
-            </div>
-        </div >
-    )
-}
+export default TopBar;
 
-export default TopBar
