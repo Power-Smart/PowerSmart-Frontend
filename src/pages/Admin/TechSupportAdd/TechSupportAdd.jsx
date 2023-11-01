3ae8f72ab7568435d61754022c25f84503bf69cf
lahiru1115 <lahirudissanayake15@gmail.com>
2023-11-01 06:38:53 +0530

    create tech support add page

--- src/pages/Admin/TechSupportAdd/TechSupportAdd.jsx
+++ src/pages/Admin/TechSupportAdd/TechSupportAdd.jsx
@@ -1,44 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import PageWrapper from "../../../components/Wrappers/PageWrapper";
 import ContentWrapper from "../../../components/Wrappers/ContentWrapper";
-import Sidebar from "../../../components/Sidebar/Sidebar";
 import TopBar from "../../../components/smallComps/TopBar";
 import PageContent from "../../../components/Wrappers/PageContent";
 import Form from "../../../components/Forms/Form";
 import FormGroup from "../../../components/Forms/FormGroup";
-import FormRowDual from "../../../components/Forms/FormRowDual";
 import TextInput from "../../../components/Forms/TextInput";
 import FormTitle from "../../../components/Forms/FormTitle";
 import FormSubmitButton from "../../../components/Forms/FormSubmitButton";
 import AdmninSidebar from "../../../components/Sidebar/Admin/AdminSidebar";
+import { addTechSupport } from "../../../api/apiAdmin";
 
 const TechSupportAdd = () => {
-  const handleSubmit = (e) => {
+  const [techSupportData, setTechSupportData] = useState({
+    user_id: "",
+    profile_pic: "",
+  });
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    alert("Form Submitted");
+    try {
+      // Make an API call to add tech support
+      const response = await addTechSupport(techSupportData);
+      alert("Tech Support Added Successfully");
+      // Reset the form or redirect as needed
+    } catch (error) {
+      console.error("Error adding tech support:", error);
+      alert("Failed to add tech support");
+    }
   };
 
   const resetForm = (e) => {
     e.preventDefault();
-    alert("Form Reset");
+    // Reset the form fields by updating the state
+    setTechSupportData({
+      user_id: "",
+      profile_pic: "",
+    });
+  };
+
+  // Update the state when the input fields change
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setTechSupportData({
+      ...techSupportData,
+      [name]: value,
+    });
   };
 
   return (
     <PageWrapper>
       <AdmninSidebar />
       <PageContent>
-        <TopBar
-          image="https://avatars.githubusercontent.com/u/73744585?v=4"
-          title="Tech Support"
-        />
+        <TopBar title="Tech Support" />
         <ContentWrapper>
           <Form>
             <FormTitle>Add New Tech Support</FormTitle>
             <FormGroup>
-              <TextInput type="text" label="Name" required={true} />
+              <TextInput
+                type="text"
+                label="user_id"
+                name="user_id"
+                value={techSupportData.user_id}
+                onChange={(e) => setTechSupportData({ ...techSupportData, user_id: e.target.value })}
+                required={true}
+              />
             </FormGroup>
             <FormGroup>
-              <TextInput type="text" label="Email" required={true} />
+              <TextInput
+                type="text"
+                label="profile_pic"
+                name="profile_pic"
+                value={techSupportData.profile_pic}
+                onChange={(e) => setTechSupportData({ ...techSupportData, profile_pic: e.target.value })}
+                required={true}
+              />
             </FormGroup>
             <div className="button-section w-2/3 text-center p-2 m-auto flex space-x-20 align-middle mt-8">
               <FormSubmitButton
